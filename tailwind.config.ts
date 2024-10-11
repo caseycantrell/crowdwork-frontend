8695b68d42e1686d846eb780f4d8bdf1e71065ba
Casey Cantrell <cantrellcasey@gmail.com>
2024-10-11 11:43:09 -0500

    cleanup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,9 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/hooks/**/*.{js,ts,jsx,tsx}',  // Optional: if you're using hooks
-    './src/utils/**/*.{js,ts,jsx,tsx}',  // Optional: if you're using utils
-    './src/styles/**/*.{css}',           // For any custom CSS
+    './src/hooks/**/*.{js,ts,jsx,tsx}',
+    './src/utils/**/*.{js,ts,jsx,tsx}',
+    './src/styles/**/*.{css}',
   ],
   theme: {
     extend: {},
