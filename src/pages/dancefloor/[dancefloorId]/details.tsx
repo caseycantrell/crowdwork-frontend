90e0c3b0126d19586ad0b87a5caa2af74bda121c
Casey Cantrell <cantrellcasey@gmail.com>
2024-11-05 19:27:40 -0600

    confirmation modal

--- src/pages/dancefloor/[dancefloorId]/details.tsx
+++ src/pages/dancefloor/[dancefloorId]/details.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import Button from '../../../components/UI/Button'
 import Modal from '../../../components/UI/Modal'
 import { formatDate } from 'date-fns';
+import { AnimatePresence, motion } from 'framer-motion';
 
 interface DJ {
   id: string;
@@ -41,10 +42,10 @@ interface Dancefloor {
 const DancefloorDetails: React.FC = () => {
   const router = useRouter();
   const { dancefloorId } = router.query;
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [statusMessage, setStatusMessage] = useState<string | null>(null);
-
-  const [dancefloor, setDancefloor] = useState<Dancefloor | null>(null);
+  const [ isModalOpen, setIsModalOpen ] = useState(false);
+  const [ statusMessage, setStatusMessage ] = useState<string | null>(null);
+  const [ isStatusMessageError, setIsStatusMessageError ] = useState<boolean>(false);
+  const [ dancefloor, setDancefloor ] = useState<Dancefloor | null>(null);
   const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
   useEffect(() => {
@@ -59,6 +60,7 @@ const DancefloorDetails: React.FC = () => {
   }, [dancefloorId, backendUrl]);
 
   const handleConfirmReactivateDancefloor = async () => {
+    setIsStatusMessageError(false);
     if (backendUrl && dancefloorId) {
       try {
         const res = await fetch(`${backendUrl}/api/dancefloor/${dancefloorId}/reactivate`, {
@@ -76,20 +78,19 @@ const DancefloorDetails: React.FC = () => {
         } else {
           setStatusMessage('Failed to reactivate dancefloor.');
         }
-      } catch (error) {
-        console.error('Error reactivating dancefloor:', error);
+      } catch {
+        setIsStatusMessageError(true);
         setStatusMessage('An error occurred while reactivating the dancefloor.');
       }
     }
   };
 
-
   if (!dancefloor) return <p className="text-center mt-10 text-gray-600">Loading...</p>;
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-800 p-6">
-      <div className="w-full max-w-6xl bg-gray-700 shadow-lg rounded-lg p-8 space-y-8">
-        <div className="flex flex-col md:flex-row md:justify-between md:items-center space-y-4 md:space-y-0">
+    <div className="min-h-screen flex items-center justify-center bg-gray-800 p-4">
+      <div className="w-full max-w-6xl bg-gray-700 shadow-lg rounded-lg p-8">
+        <div className="flex flex-col md:flex-row md:justify-between md:items-center pb-2">
           <p className="text-2xl font-bold text-white">Dancefloor ID: {dancefloorId}</p>
           <Link href={`/dj/${dancefloor.dj_id}`}>
             <Button 
@@ -100,61 +101,73 @@ const DancefloorDetails: React.FC = () => {
             </Button>
           </Link>
         </div>
-
-        <div className="p-6 bg-gray-600 rounded-lg text-white">
-          <div className="flex flex-col md:flex-row md:justify-between md:items-center">
-            <p className="text-2xl font-semibold mb-4">Dancefloor Info</p>
-            <Button 
-              onClick={() => setIsModalOpen(true)} 
-              bgColor="bg-green-500 hover:bg-green-600" 
-              padding="px-4 py-2"
-              className="ml-auto"
-            >
-              Reactivate Dancefloor
-            </Button>
-          </div>
+        <div className="p-6 bg-gray-600 flex flex-row items-center justify-between rounded-lg text-white mb-8 mt-4">
+          <div>
+            <div className="flex flex-col md:flex-row md:justify-between md:items-center">
+              <p className="text-2xl font-semibold mb-4">Dancefloor Info</p>
+            </div>
             <div className='flex flex-row items-center'>
               <p className='font-bold'>Status:</p> 
               <p className='font-medium ml-1'>{dancefloor.status}</p>
             </div>
-          <div className="flex flex-col">
-            <div className='flex flex-row items-center'>
-              <p className='font-bold'>Created At:</p> 
-              <p className='font-medium ml-1'>{new Date(dancefloor.created_at).toLocaleString()}</p>
-            </div>
-            <div className='flex flex-row items-center'>
-              <p className='font-bold'>Ended At:</p> 
-              <p className='font-medium ml-1'>{new Date(dancefloor.ended_at).toLocaleString()}</p>
-            </div>
-            <div className='flex flex-row items-center'>
-              <p className='font-bold'>Total Requests:</p> 
-              <p className='font-medium ml-1'>{dancefloor.requests_count}</p>
-            </div>
-            <div className='flex flex-row items-center'>
-              <p className='font-bold'>Total Messages:</p> 
-              <p className='font-medium ml-1'>{dancefloor.messages_count}</p>
+            <div className="flex flex-col">
+              <div className='flex flex-row items-center'>
+                <p className='font-bold'>Created At:</p> 
+                <p className='font-medium ml-1'>{new Date(dancefloor.created_at).toLocaleString()}</p>
+              </div>
+              <div className='flex flex-row items-center'>
+                <p className='font-bold'>Ended At:</p> 
+                <p className='font-medium ml-1'>{new Date(dancefloor.ended_at).toLocaleString()}</p>
+              </div>
+              <div className='flex flex-row items-center'>
+                <p className='font-bold'>Total Requests:</p> 
+                <p className='font-medium ml-1'>{dancefloor.requests_count}</p>
+              </div>
+              <div className='flex flex-row items-center'>
+                <p className='font-bold'>Total Messages:</p> 
+                <p className='font-medium ml-1'>{dancefloor.messages_count}</p>
+              </div>
             </div>
           </div>
-        </div>
 
+          <div className='mr-16'>
+            <Button 
+              onClick={() => setIsModalOpen(true)} 
+              bgColor="bg-green-500" 
+              padding="px-16 py-5"
+              className='text-lg'
+            >
+              Reactivate Dancefloor
+            </Button>
+          </div>
+        </div>
+        {/* confirmation modal */}
          <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-          <div className="p-4 space-y-4">
-            <p className="text-2xl font-bold">Reactivate Dancefloor</p>
-            <p className="font-semibold">Are you sure you want to reactivate this dancefloor? This will override any currently active dancefloor.</p>
-            
+          <div className="p-4 relative space-y-4 pb-5">
+            <p className="text-2xl font-bold">Reactivate Dancefloor?</p>
+           <div className='pb-2'>
+            <p className="font-semibold">Are you sure you want to reactivate this dancefloor?</p>
+            <p className="font-semibold">This will override any currently active dancefloor.</p>
+           </div>
             <Button
               onClick={handleConfirmReactivateDancefloor}
-              bgColor="bg-green-500"
-              className="w-full"
+              className="w-full mt-2 text-lg"
+              padding='py-4'
             >
               Confirm Reactivate
             </Button>
-
-            {statusMessage && (
-              <p className={`text-center mt-4 ${statusMessage.includes('successfully') ? 'text-green-500' : 'text-red-500'}`}>
+            <AnimatePresence>
+              {statusMessage && 
+                <motion.p
+                  initial={{ opacity: 0, x: -20 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: -10 }}
+                  transition={{ type: 'tween', duration: 0.5, ease: 'easeInOut' }}
+                  className={`${isStatusMessageError ? 'text-red-500 ' : 'text-green-500 '} text-center font-semibold text-sm absolute -bottom-3 left-0 right-0 `}>
                 {statusMessage}
-              </p>
-            )}
+                </motion.p>
+              }
+            </AnimatePresence>
           </div>
         </Modal>
 
