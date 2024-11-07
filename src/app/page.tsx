e8641f8d599f0dddc143aa6681d7b3697ac20ba4
Casey Cantrell <cantrellcasey@gmail.com>
2024-11-07 11:35:49 -0600

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,76 @@
-export default function Home() {
+"use client"
+
+import { useRef, useEffect } from 'react';
+import '../../src/styles/gradient-bg.css';
+import Link from 'next/link';
+
+const Home: React.FC = () => {
+  const interactiveRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    let curX = 0;
+    let curY = 0;
+    let tgX = 0;
+    let tgY = 0;
+
+    function move() {
+      curX += (tgX - curX) / 20;
+      curY += (tgY - curY) / 20;
+
+      if (interactiveRef.current) {
+        interactiveRef.current.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+      }
+      requestAnimationFrame(move);
+    }
+
+    const handleMouseMove = (event: MouseEvent) => {
+      tgX = event.clientX;
+      tgY = event.clientY;
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    move();
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-       <p className="text-6xl font-bold">#CRWDWRK</p>
-      </main>
-     
+    <div className=''>
+      <div className="text-container">
+        <p className='font-bold'>CROWDWORK</p>
+      </div>
+      <div className="gradient-bg flex flex-col min-h-screen">
+        <div className='login-signup-container'>
+          <Link href='/login' className='font-bold hover:text-main ease-in-out duration-300 cursor-pointer'>Login</Link>
+          <Link href='/signup' className='font-bold hover:text-main ease-in-out duration-300 cursor-pointer ml-6'>Signup</Link>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <filter id="goo">
+              <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+              <feColorMatrix
+                in="blur"
+                mode="matrix"
+                values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
+                result="goo"
+              />
+              <feBlend in="SourceGraphic" in2="goo" />
+            </filter>
+          </defs>
+        </svg>
+        <div className="gradients-container">
+          <div className="g1"></div>
+          <div className="g2"></div>
+          <div className="g3"></div>
+          <div className="g4"></div>
+          <div className="g5"></div>
+          <div ref={interactiveRef} className="interactive"></div>
+        </div>
+      </div>
     </div>
   );
 };
+
+export default Home;
