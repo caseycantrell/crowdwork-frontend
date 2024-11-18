38fb5da408412efbd0a1d7cf322a83bb46a318d5
Casey Cantrell <cantrellcasey@gmail.com>
2024-11-18 15:43:03 -0600

    trying bg blur

--- src/pages/test2.tsx
+++ src/pages/test2.tsx
@@ -3,7 +3,7 @@ const Test = () => {
     <div className="min-h-screen flex flex-col justify-center items-center relative">
       <div className="gradient-background-variation"></div>
       <div className="w-[60rem] h-[20rem] bg-gray-800/50 rounded-lg flex flex-col justify-center items-center font-bold p-8 backdrop-blur">
-        <p className="text-4xl">some other stuff</p>
+        <p className="text-4xl">some other stuff yo</p>
       </div>
     </div>
     );
