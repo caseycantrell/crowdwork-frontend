ffe1282eb37a61d2ee87ac8270837da6e9d1feed
Casey Cantrell <cantrellcasey@gmail.com>
2024-11-21 16:44:13 -0600

    font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Advent_Pro } from "next/font/google";
 import "../styles/globals.scss";
 
 const geistSans = localFont({
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
